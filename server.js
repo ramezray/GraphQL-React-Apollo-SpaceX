d83335dc541322e2b4d7ffce91903fe5d29c6990
unknown <ray_ramez@yahoo.com>
2020-07-13 19:01:07 -0700

    working on heroku

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ app.use(
     graphiql: true,
   })
 );
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+app.use(express.static("public"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+});
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, console.log(`server is start on port ${PORT}`));

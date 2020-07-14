7eb5175f0405bb5e7e1b844cc71baf34110bb1b8
unknown <ray_ramez@yahoo.com>
2020-07-13 19:41:37 -0700

    working on heroku

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
 const path = require("path");
-const cors = require("cors");
 
 const app = express();
 
-//to fix the cors issue when run local
 app.use(cors());
 
 app.use(
@@ -16,12 +15,13 @@ app.use(
     graphiql: true,
   })
 );
+
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server is start on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

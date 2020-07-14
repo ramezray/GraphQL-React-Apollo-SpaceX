b35db73b3c26c952764bfafb8dd2e698d85613c5
unknown <ray_ramez@yahoo.com>
2020-07-13 19:30:57 -0700

    working on heroku

---
+++ public/build/precache-manifest.d115bbe7e40be9e45628e4f3ae00a2b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59a4a2ff64f20aeb5a91d0846ac4266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9311b7bc8f0240bab48",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "839c50b2af68bd866bee",
+    "url": "/static/js/2.37cc463d.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.37cc463d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9311b7bc8f0240bab48",
+    "url": "/static/js/main.f108c3e0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dd9a9d7a83728bdfa264a46b31979724",
+    "url": "/static/media/logo.dd9a9d7a.png"
+  }
+]);
